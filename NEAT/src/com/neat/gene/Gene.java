771a33f42512529e1d623b88ad14c7281c3077bc
Burak Sefa Sert <bsefasert@gmail.com>
2017-07-23 01:02:31 +0300

    Resolve merge conflict

--- NEAT/src/com/neat/gene/Gene.java
+++ NEAT/src/com/neat/gene/Gene.java
@@ -3,14 +3,9 @@ package com.neat.gene;
 import com.sun.tools.javac.util.Pair;
 
 public interface Gene {
-<<<<<<< HEAD
     public int getNodeI();
 
     public int getNodeO();
-=======
-
-    public Pair<Integer,Integer> getNodes();
->>>>>>> 80dde7dbad7961d22483944e4ad4874cc009cbd5
 
     public boolean equals(Gene gene);
 
