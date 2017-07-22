5ec95b766187a6578d1cc5b9f911f2b82cc1ec1a
Burak Sefa Sert <bsefasert@gmail.com>
2017-07-23 00:36:19 +0300

    Gene interface methods added

--- NEAT/src/com/neat/gene/Gene.java
+++ NEAT/src/com/neat/gene/Gene.java
@@ -1,5 +1,12 @@
 package com.neat.gene;
 
+import com.sun.tools.javac.util.Pair;
+
 public interface Gene {
+    public Pair<Integer,Integer> getNodes();
+
+    public boolean equals(Gene gene);
+
+    public int getInnovationNumber();
 
 }
